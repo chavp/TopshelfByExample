a90a09fb9440683cc0acbb70bb347b81cf740f62
chavp <my.parinya@gmail.com>
2014-06-17 10:37:36 +0700

    Create Windows Services with Topshelf
    Signed-off-by: chavp <my.parinya@gmail.com>

---
+++ TopshelfByExample/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TopshelfByExample
+{
+    using Topshelf;
+
+    class Program
+    {
+        /// <summary>
+        ///  http://msdn.microsoft.com/en-us/magazine/dn745865.aspx
+        ///  http://docs.topshelf-project.com/en/latest/overview/commandline.html
+        /// </summary>
+        /// <param name="args"></param>
+        static void Main(string[] args)
+        {
+            HostFactory.Run(host =>
+            {
+                host.Service<TownCrier>(service =>
+                {
+                    service.ConstructUsing(() => new TownCrier());
+
+                    service.WhenStarted(a => a.Start());
+                    service.WhenStopped(a => a.Stop());
+
+                    //service.WhenPaused(s => s.Pause());
+                    //service.WhenContinued(s => s.Continue());
+
+                    //service.WhenShutdown(s => s.Shutdown());
+                });
+
+                host.RunAsLocalSystem();                            //6
+
+                host.SetDescription("Sample Topshelf Host");        //7
+                host.SetDisplayName("Stuff");                       //8
+                host.SetServiceName("stuff");                       //9
+            });
+        }
+    }
+}
---
+++ TopshelfByExample/TownCrier.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Timers;
+
+namespace TopshelfByExample
+{
+    public class TownCrier
+    {
+        readonly Timer _timer;
+        public TownCrier()
+        {
+            _timer = new Timer(1000) { AutoReset = true };
+            _timer.Elapsed += (sender, eventArgs) => Console.WriteLine("It is {0} an all is well", DateTime.Now);
+        }
+        public void Start() { _timer.Start(); }
+        public void Stop() { _timer.Stop(); }
+    }
+}
